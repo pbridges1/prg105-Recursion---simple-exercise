868b68077dd984c0f80dd0ea452ff9ce61d4df69
pbridges1 <pbridges@students.mchenry.edu>
2016-09-13 18:10:50 -0500

    Add files via upload

---
+++ beer_song.py
@@ -0,0 +1,15 @@
+
+def num_beers(n):
+    str(num_beers)
+    str(n)
+    n = 100
+    for i in range(100):
+        n = (n - 1)
+
+        if n > 0:
+            print n, "bottles of beer on the wall "
+            print n, "bottles of beer"
+            print "If one of those bottles should happen to fall"
+            print n-1, "bottles of beer on the wall \n"
+
+num_beers(99)
